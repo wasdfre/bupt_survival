d318e9acf6a7ba22a26730ff12e1ebf875882d1d
wasdfre <2247246336@qq.com>
2022-04-26 21:27:15 +0800

    cc

--- pages/discovery/discovery.js
+++ pages/discovery/discovery.js
@@ -1,18 +1,17 @@
 //discovery.js
-const app = getApp()
-var util = require('../../utils/util.js')
+//常参数设定
+const app = getApp()//创建APP类的实例
+var util = require('../../utils/util.js')//调用其它文件模块
 const db = wx.cloud.database()//初始化数据库 宏定义一个db指代
-const _ = db.command;//Assistant_DataSheet表
-var data = {
-  //云开发环境id
-  env: 'a123-4gjil6fj4c251504'
-  }
+const _ = db.command;//宏定义一个_指代数据库操作符
+var data = {env: 'a123-4gjil6fj4c251504'}//云开发环境id
 Page({
   data: {
     navTab: ["最新", "热门",],      //切换类别
     currentNavtab: "0",            //目前类别
-    //轮播图参数
-    imgUrls: [//图片列表
+    //轮播图图片列表
+    imgUrls: 
+    [
       '../../images/24213.jpg',
       '../../images/24280.jpg',
       '../../images/1444983318907-_DSC1826.jpg'
@@ -23,113 +22,130 @@ Page({
     duration: 1000,
     feed: [],
     feed_length: 0,
-    //问题内容列表
+    //按时间排序问题内容列表
     DataPostArry:[], 
-    DataPostArry_Heat:[], 
-    //用户头像列表             
-    UserHeadurlArry:[],    
-    //点赞信息列表
-    UpArray: [],
-    //用户名列表                    
-    UsernameArry: [],   
-    //用户openid列表            
-    UserId:'',                      
+    //按热度排序问题内容列表
+    DataPostArry_Heat:[],                         
     //回复信息列表
     replyData: []   
   },
-  //一载入页面
-  sort_index:function (a,b){
-    a_heat=a.Up_Record_num*0.3+a.Reply_Record_num*0.7;
-    b_heat=b.Up_Record_num*0.3+b.Reply_Record_num*0.7;
-    return a.Up_Record_num*0.3+a.Reply_Record_num*0.7-b.Up_Record_num*0.3+b.Reply_Record_num*0.7;
+
+  
+  //切换上方选择页面的函数
+  switchTab: function(e)
+  {
+    this.setData
+    ({
+      currentNavtab: e.currentTarget.dataset.idx
+    });
   },
+
+
+  //点击某个问题后对应动作
+  bindQueTap: function(e)
+  {
+    let that = this
+    // console.log(1111,e)
+    //缓存点击问题的序号，postid，问题者openid
+    wx.setStorage
+    ({
+      key: "key",
+      data:  e.currentTarget.dataset
+    })
+    //跳转至对应回答显示界面
+    wx.navigateTo
+    ({
+      url: '../question/question'
+    })
+  },
+
+
+  // 排序函数
+  sort_heat:function (a,b)
+  {
+    var heat_a=a.Up_Record_num*0.3+a.Reply_Record_num*0.7;
+    var heat_b=b.Up_Record_num*0.3+b.Reply_Record_num*0.7;
+    return heat_b-heat_a;
+  },
+
+
+  //获得问题数据
+  get_question:function()
+  {
+    const db = wx.cloud.database()
+    //获取全部数据
+    db.collection('Assistant_DataSheet').get
+    ({
+      success: res => 
+      {
+        this.setData
+        ({
+          DataPostArry: res.data
+        })
+        this.get_uesrdata();
+      }
+    })
+  },
+
+
+  //获取发布问题人数据
+  get_uesrdata:function()
+  {
+    var res_temp=this.data.DataPostArry;
+    console.log('dwdawdwa',res_temp)
+    Promise.all(res_temp.map((item)=>item["_openid"])).then
+    (
+      res=>
+      {
+        //根据所有用户的openid获取用户数据，在数据库Assistant_User
+        db.collection('Assistant_User').where
+        ({
+          _openid:_.in(res)//根据在res中的openid检索数据
+        }).get().then
+        (
+          res => 
+          {
+            // 为每一条问题添加用户头像与姓名
+            for (let i = 0; i < res_temp.length;i++)
+            {
+              //根据每个问题的open进行匹配
+              let openId = res_temp[i]._openid;
+              for(let j=0;j<res.data.length;j++)
+              {
+                if(openId == res.data[j]._openid)
+                {
+                  res_temp[i]['Username']=res.data[j].Username;
+                  res_temp[i]['UserHeadurl']=res.data[j].User_head_url;
+                }
+              }
+            }       
+            this.setData
+            ({
+              DataPostArry: res_temp.reverse(),//按照时间排序
+              DataPostArry_Heat: res_temp.sort(this.sort_heat)//按照热度排序
+            });
+          }
+        )
+      }
+    ).catch((ex)=>
+      {
+        console.log(ex);
+      }
+    )
+  },
+
+  //启动函数
   onLoad: function () 
   {
     // this.gethis();
     // this.getnew();
-    //获取正在使用的用户信息
-    wx.getStorage
-    ({
-      key: 'Userinfo',
-    })
-    //获取问题信息，在数据库Assistant_DataSheet中
-    var that = this
-    const get_inf_db = wx.cloud.database()
-    get_inf_db.collection('Assistant_DataSheet').get
-    ({
-      success: res => 
-      {
-        var res_temp=res.data.reverse();
-        
-        that.setData
-        ({
-          DataPostArry: res_temp//获取全部数据
-        })
-        
-        //异步promise对象，不用管
-        //map，每个对象调用一个函数，item赋为空值
-        Promise.all(res_temp.map((item)=>item["_openid"])).then
-        (
-          res=>
-          {
-            var postdata=res_temp;
-            console.log("11113dwadw23232",postdata)
-            //这里是_定
-            let _ = get_inf_db.command;
-            // console.log('dadwadaw',res.data.map((item)=>item[key]))
-            //根据所有用户的openid获取用户数据，在数据库Assistant_User
-            // console.log("头像ers",res)
-            get_inf_db.collection('Assistant_User').where
-            ({
-              // _openid: "oSe9A5dvjrhoLCUz4pAKmN9487PY"
-              _openid:_.in(res)
-              // _.in(res)//在res中的openid
-            }).get().then
-            (res => 
-              {
-                
-                // console.log("头像",res)
-                // that.data.UsernameArry = [];//用户名
-                // that.data.UserHeadurlArry=[];//头像
-                // console.log("1111232",res)
-                for (let i = 0; i < postdata.length;i++)
-                {
-                  //已知问题列表中每个问题对应的openid
-                  let openId = postdata[i]._openid;
-                  for(let j=0;j<res.data.length;j++)
-                  {
-                    if(openId == res.data[j]._openid)
-                    {//获取对应用户名和头像
-                      postdata[i]['Username']=res.data[j].Username;
-                      //这里有什么区别
-                      postdata[i]['UserHeadurl']=res.data[j].User_head_url;
-                      // that.data.UsernameArry.push(res.data[j].Username);
-                      // that.data.UserHeadurlArry.push(res.data[j].User_head_url);
-                    }
-                  }
-                }
-                
-                // console.log("11113dwadw23232",postdata)
-                that.setData
-                ({
-                  DataPostArry: postdata,
-                  DataPostArry_Heat: postdata.sort(function (a,b)
-                  {
-                    return b.Up_Record_num*0.3+b.Reply_Record_num*0.7-a.Up_Record_num*0.3+a.Reply_Record_num*0.7;
-                  },)
-                  // UserHeadurlArry: that.data.UserHeadurlArry
-                });
-                console.log("头像",that.data.DataPostArry_Heat)
-              }
-            )
-          }).catch((ex)=>
-          {
-            console.log(ex);
-          }
-        )  
 
-       }
-     })
+
+    //从数据库Assistant_DataSheet中，获取问题列表
+      // 获取用户数据嵌套在获取问题列表后的success中
+    this.get_question();
+
+
     // this.refresh();
   },
   
@@ -264,35 +280,11 @@ onPageScroll: function (e) {
 
 
 
-  //切换上方选择页面的函数
-  switchTab: function(e)
+
+
+
+  search_content(event)
   {
-    this.setData({
-      currentNavtab: e.currentTarget.dataset.idx
-    });
-  },
-  //点击问题对应的函数
-  bindQueTap: function(e)
-  {
-    let that = this
-    //获取当前点击问题的postid，问题者openid
-    that.setData
-    ({
-      replyData: e.currentTarget.dataset
-    })
-    console.log(that.data.replyData)
-    // 缓存相关信息
-    wx.setStorage
-    ({
-      key: "key",
-      data: that.data.replyData
-    })
-    //跳转页面
-    wx.navigateTo({
-      url: '../question/question'
-    })
-  },
-  search_content(event){
     console.log("搜索内容：", event.detail.value)    //这里可以让我们在开发的时候在console控制台上看到我们的结果
     Rname1 = event.detail.value //这里的Rname1 也是需要在前面进行宏定义的格式为（ let Rname1 = "" ） ，然后给它赋值
   },
